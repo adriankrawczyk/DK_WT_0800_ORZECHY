c99c723d3a020be24e2271aa189dedbdcba51a99
alekmatyka <amatyka@student.agh.edu.pl>
2026-01-18 00:34:03 +0100

    doctor repo - find by spec

--- src/main/java/com/example/nfz/repository/DoctorRepository.java
+++ src/main/java/com/example/nfz/repository/DoctorRepository.java
@@ -1,9 +1,14 @@
 package com.example.nfz.repository;
 
 import com.example.nfz.model.Doctor;
+import com.example.nfz.model.Specialization;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
+
+    List<Doctor> findAllDoctorsBySpecialization(Specialization specialization);
 }
